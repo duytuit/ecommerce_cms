4243b6dda73eac80b070d122be0dc6ec766bf0a4
Duy Tu <duytu89@gmail.com>
2023-04-20 12:54:51 +0700

    dev

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -975,7 +975,7 @@ if (!function_exists('static_asset')) {
 if (!function_exists('getBaseURL')) {
     function getBaseURL()
     {
-        $root = '//' . $_SERVER['SERVER_NAME'];
+        $root = '//' . $_SERVER['HTTP_HOST'];
         $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
         return $root;
